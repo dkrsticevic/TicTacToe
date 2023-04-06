7d2cf71b2a8e0ece45b7639d9bfbdc9c8b18658c
Daniel Krsticevic <danielkrsticevic@gmail.com>
2023-04-06 21:13:25 +0800

    fixing routing problems

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App'
 import "bootstrap/dist/css/bootstrap.min.css"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-      <Router>
+      <Router basename='TicTacToe'>
         <App />
       </Router>
 )
