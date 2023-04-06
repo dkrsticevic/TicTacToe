9b9de0c293348933fc076a593a6f5523ad41495b
Daniel Krsticevic <danielkrsticevic@gmail.com>
2023-04-06 21:52:56 +0800

    f

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: "/TicTacToe/", // remove when running locally
+  base: "/TicTacToe/", // remove when running locally
 })
